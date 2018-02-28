f901adc9c6d352bbb66942cf940e698b5096cc4b
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-02-28 12:28:07 -0800

    wrote the updateItem api method

--- scripts/api.js
+++ scripts/api.js
@@ -4,10 +4,12 @@
 //eslint-disable-next-line no-unused-vars
 const api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/tj';
+
   const getItems = function(callback){
 
     $.getJSON(`${BASE_URL}/items`, callback);//Added this to make arrays show up
   };
+
   const createItem = function(name, callback) {
     const newItem = JSON.stringify({name: name});
   
@@ -22,9 +24,20 @@ const api = (function () {
 
     $.ajax(testData);
   };
+  
+  const updateItem = function(id, updateData, callback) {
+    $.ajax({
+      url : `${BASE_URL}/items/${id}`,
+      method: 'PATCH',
+      contentType: 'application/json',
+      data: JSON.stringify(updateData),
+      success: callback
+    });
+  };
 
   return {
     getItems,
-    createItem
+    createItem,
+    updateItem
   };
 }());
